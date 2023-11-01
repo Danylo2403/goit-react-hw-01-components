35da66aad19843a685491220efa2352f5029a77c
Danylo2403 <daniilkon24@gmail.com>
2023-11-02 01:12:08 +0200

    New file

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,25 @@
+// import
+//
+import { Profile } from './Profile/profile';
+import user from '../data/user.json';
+//
+// import { Statistics } from './Statistics/statistics';
+// import user from '../data/data.json';
+// //
+// import { FriendList } from './FriendList/friendList';
+// import user from '../data/friends.json';
+// //
+// import { TransactionHistory } from './TransactionHistory/transactionHistory';
+// import user from '../data/transactions.json'
+//
+//export const app
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      react
+    <div>
+      <Profile user={user} />
+      <Statistics title="Upload stats" stats={data} />
+      <FriendList friends={friend} />
+      <TransactionHistory items={transactions} />
     </div>
   );
 };
---
+++ src/components/Profile/profile.jsx
@@ -0,0 +1,48 @@
+import {
+  ProfileWrapper,
+  Description,
+  Avatar,
+  Name,
+  Tag,
+  Location,
+  StatsList,
+  Item,
+  Label,
+  Quantity,
+} from './profile.styled';
+
+export const Profile = ({
+  user: {
+    username,
+    tag,
+    location,
+    avatar,
+    stats: { followers, views, likes },
+  },
+}) => {
+  return (
+    <Profile>
+      <Description>
+        <Avatar src={avatar} alt="User avatar" />
+        <Name>{username}</Name>
+        <Tag>@{tag}</Tag>
+        <Location>{location}</Location>
+      </Description>
+
+      <StatsList>
+        <Item>
+          <Label>Followers</Label>
+          <Quantity>{followers}</Quantity>
+        </Item>
+        <Item>
+          <Label>Views</Label>
+          <Quantity>{views}</Quantity>
+        </Item>
+        <Item>
+          <Label>Likes</Label>
+          <Quantity>{likes}</Quantity>
+        </Item>
+      </StatsList>
+    </Profile>
+  );
+};
